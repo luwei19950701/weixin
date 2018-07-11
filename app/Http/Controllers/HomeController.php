4a1b055dfe8aa5def187ed0492bc54d37f26f125
luwei <1197329636@qq.com>
2018-07-11 13:21:15 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use EasyWeChat\Factory;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +25,23 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        $config = [
+            'app_id' => 'wx2e66e13530979fa2',
+            'secret' => '2424a3425e17e193268bb0c081b7cc8a',
+
+            // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+            'response_type' => 'array',
+
+//            'log' => [
+//                'level' => 'debug',
+//                'file' => __DIR__.'/wechat.log',
+//            ],
+        ];
+
+        $app = Factory::officialAccount($config);
+
+        dd($app);
         return view('home');
     }
 }
